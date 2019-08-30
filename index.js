bafb614d304c9851fec9d69a02a83ec9b8f71eba
Halil güler <h.guler3717@gmail.com>
2019-08-30 13:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const driver = 'bob';
 
-console.log ('This code was called');
+/*console.log ('This code was called');
 
-/*console.log ( driver );
+console.log ( driver );
 
 console.log ( driver 'is the driver variable name');
